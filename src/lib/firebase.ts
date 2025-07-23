97816e42fa28610d57d35f0d4c436f6df74d5fd9
Joseph Llanes <llanes.joseph.m@gmail.com>
2025-07-23 20:06:45 +0000

    that should go in the EVN file beacus ei t has back end infomation

--- src/lib/firebase.ts
+++ src/lib/firebase.ts
@@ -10,13 +10,13 @@ import { getStorage } from "firebase/storage";
 // Your web app's Firebase configuration
 // For Firebase JS SDK v7.20.0 and later, measurementId is optional
 const firebaseConfig = {
-  apiKey: "AIzaSyA7tvTOqf70qMkWmunwKouqw51XaWYf7Ak",
-  authDomain: "kaya-4e1c4.firebaseapp.com",
-  projectId: "kaya-4e1c4",
-  storageBucket: "kaya-4e1c4.appspot.com",
-  messagingSenderId: "969650140042",
-  appId: "1:969650140042:web:6405696891f4cbec360e73",
-  measurementId: "G-B84919JMJM"
+  apiKey: process.env.NEXT_PUBLIC_FIREBASE_API_KEY,
+  authDomain: process.env.NEXT_PUBLIC_FIREBASE_AUTH_DOMAIN,
+  projectId: process.env.NEXT_PUBLIC_FIREBASE_PROJECT_ID,
+  storageBucket: process.env.NEXT_PUBLIC_FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: process.env.NEXT_PUBLIC_FIREBASE_MESSAGING_SENDER_ID,
+  appId: process.env.NEXT_PUBLIC_FIREBASE_APP_ID,
+  measurementId: process.env.NEXT_PUBLIC_FIREBASE_MEASUREMENT_ID
 };
 
 // Initialize Firebase
