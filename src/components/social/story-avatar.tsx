08d41ada78256f23e3b368e69d497460ba2e9e52
Joseph Llanes <llanes.joseph.m@gmail.com>
2025-07-24 01:10:50 +0000

    i dont like it  i need a something that idicates the cannabis and  growe

--- src/components/social/story-avatar.tsx
+++ src/components/social/story-avatar.tsx
@@ -6,27 +6,27 @@ interface StoryAvatarProps {
 }
 
 export function StoryAvatar({ story }: StoryAvatarProps) {
-  const organicShapeClipPath = 'path("M48.2,1.8C68.5,5.8,78.6,24.3,80,45.4s-11.4,41-31.8,42.8S6.6,83.1,2.2,62.2,12.5,12.3,27.9,4.2,39.3,-0.1,48.2,1.8Z")';
+  const leafClipPath = 'path("M50 2C52.5 7.5 55 15 55 25C55 30 54 35 52 40C58 36 65 35 70 35C75 35 80 37 85 41C81 44 78 48 75 52C78 54 81 57 84 60C79 63 74 65 70 66C72 71 75 76 75 82C75 88 73 94 70 98H30C27 94 25 88 25 82C25 76 28 71 30 66C26 65 21 63 16 60C19 57 22 54 25 52C22 48 19 44 15 41C20 37 25 35 30 35C35 35 42 36 48 40C46 35 45 30 45 25C45 15 47.5 7.5 50 2Z")';
 
   return (
     <div className="flex flex-col items-center space-y-2">
       <div
-        className="relative w-[82px] h-[90px] flex items-center justify-center"
+        className="relative w-[100px] h-[100px] flex items-center justify-center"
       >
         <div
             className="absolute inset-0 bg-primary"
-            style={{ clipPath: organicShapeClipPath }}
+            style={{ clipPath: leafClipPath }}
         ></div>
         <div
-            className="relative w-[76px] h-[84px] bg-background"
-            style={{ clipPath: organicShapeClipPath }}
+            className="relative w-[92px] h-[92px] bg-background"
+            style={{ clipPath: leafClipPath }}
         >
           <Image
             src={story.user.avatarUrl}
             alt={story.user.name}
             fill
             className="object-cover"
-            style={{ clipPath: organicShapeClipPath }}
+            style={{ clipPath: leafClipPath }}
             data-ai-hint="user avatar"
           />
         </div>
