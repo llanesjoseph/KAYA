d2a35a36094141de87c91f3c7782e80d89649a28
Joseph Llanes <llanes.joseph.m@gmail.com>
2025-07-24 01:07:31 +0000

    can we change these to somthings more earth  this look to clean so we ne

--- src/components/social/story-avatar.tsx
+++ src/components/social/story-avatar.tsx
@@ -8,14 +8,23 @@ interface StoryAvatarProps {
 export function StoryAvatar({ story }: StoryAvatarProps) {
   return (
     <div className="flex flex-col items-center space-y-2">
-      <div className="relative rounded-full p-1 bg-[#8A2BE2]">
-        <div className="rounded-full bg-background p-0.5">
+      <div
+        className="relative w-[72px] h-[80px] flex items-center justify-center"
+        style={{ clipPath: 'polygon(50% 0%, 100% 25%, 100% 75%, 50% 100%, 0% 75%, 0% 25%)' }}
+      >
+        <div className="absolute inset-0 bg-primary"
+             style={{ clipPath: 'polygon(50% 0%, 100% 25%, 100% 75%, 50% 100%, 0% 75%, 0% 25%)' }}
+        ></div>
+        <div className="relative w-[68px] h-[76px] bg-background"
+             style={{ clipPath: 'polygon(50% 0%, 100% 25%, 100% 75%, 50% 100%, 0% 75%, 0% 25%)' }}
+        >
           <Image
             src={story.user.avatarUrl}
             alt={story.user.name}
             width={64}
             height={64}
-            className="rounded-full object-cover"
+            className="absolute top-1/2 left-1/2 -translate-x-1/2 -translate-y-1/2 object-cover w-[64px] h-[64px]"
+            style={{ clipPath: 'polygon(50% 0%, 100% 25%, 100% 75%, 50% 100%, 0% 75%, 0% 25%)' }}
             data-ai-hint="user avatar"
           />
         </div>
