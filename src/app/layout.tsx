6b8d4aca122a3abf1cc03b2e6c70631385b06def
Joseph Llanes <llanes.joseph.m@gmail.com>
2025-07-25 15:54:12 +0000

    can we see what a lanfing page look like  please  with theme and feel of

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -23,7 +23,7 @@ export default function RootLayout({
           crossOrigin="anonymous"
         />
         <link
-          href="https://fonts.googleapis.com/css2?family=Inter:wght@400;500;700&family=Source+Serif+Pro:wght@400;700;900&display=swap"
+          href="https://fonts.googleapis.com/css2?family=Inter:wght@400;500;700&family=Nunito:wght@400;700;900&display=swap"
           rel="stylesheet"
         />
       </head>
