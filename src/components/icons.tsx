deae494fc470514f1e81d4a0c71d01e1c7fffc45
Joseph Llanes <llanes.joseph.m@gmail.com>
2025-07-27 23:38:18 +0000

    this needs to be trhe small icon i gave earlier

--- src/components/icons.tsx
+++ src/components/icons.tsx
@@ -4,19 +4,18 @@ export function KayaHubLogo(props: SVGProps<SVGSVGElement>) {
   return (
     <svg
       xmlns="http://www.w3.org/2000/svg"
-      viewBox="0 0 100 25"
+      viewBox="0 0 24 24"
+      fill="none"
+      stroke="currentColor"
+      strokeWidth="2"
+      strokeLinecap="round"
+      strokeLinejoin="round"
       {...props}
       className="fill-current text-foreground"
     >
-      <text 
-        x="0" 
-        y="20" 
-        fontFamily="Nunito, sans-serif" 
-        fontSize="24" 
-        fontWeight="bold"
-      >
-        KAYAHUB
-      </text>
+      <path d="M12 2L12 22" />
+      <path d="M17 7L12 12L17 17" />
+      <path d="M7 12H12" />
     </svg>
   );
 }
